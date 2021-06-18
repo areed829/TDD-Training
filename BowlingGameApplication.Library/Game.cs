2749388634d9e0c329346abe2540679e48817eb6
Alex Reed <areed@NEW-INNOV.COM>
2021-06-18 17:05:51 -0400

    working on bowling class

---
+++ BowlingGameApplication.Library/Game.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+
+namespace BowlingGameApplication.Library
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int rollNumber = 0;
+        private bool newFrame = true;
+        /*
+         * List<int[]> rolls
+         * roll => if rolls.last.length == 2 then list.add(new [2])
+         */
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                foreach (var roll in rolls)
+                {
+                    score += roll;
+                }
+
+                return score;
+            }
+        }
+
+        public int getScore() {
+            int score = 0;
+            int nextBall = 0;
+            for (int i = 0; i < rolls.Length; i++)
+            {
+                if (rolls[i] == 10)
+                {
+                    score += rolls[i];
+                    score += rolls[i+1];
+                    score += rolls[i+2];
+                    nextBall++;
+                }
+                else if (rolls[i] + rolls[i+1] == 10)
+                {
+                    score += 10 + rolls[i + 2];
+                    nextBall += 2;
+                }
+                else
+                {
+                    score += rolls[i] + rolls[i + 1];
+                    nextBall += 2;
+                }
+
+            }
+
+            return score;
+        }
+
+        public void Roll(int numPins)
+        {
+            rolls[rollNumber++] = numPins;
+        }
+    }
+}
+
+//[
+//    10,     20
+//    0,
+//    10      35
+//    5
+//    4       44
+//]
+
+
+//[
+//    [1: x,2: null],
+//    [1: 5,2: /],
+//    [1: 3, 2: 4]
+//]
---
+++ BowlingGameApplication.Tests/GameTests.cs
@@ -0,0 +1,55 @@
+﻿using BowlingGameApplication.Library;
+using NUnit.Framework;
+
+namespace BowlingGameApplication.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        private Game game;
+
+        private const int GUTTERBALL = 0;
+
+        [SetUp]
+        public void Setup()
+        {
+            game = new Game();
+        }
+
+        [Test]
+        public void CanRoll()
+        {
+            game.Roll(3);
+            Assert.AreEqual(3, game.Score);
+        }
+
+        [Test]
+        public void GutterGame()
+        {
+            rollMany(20, GUTTERBALL);
+            Assert.AreEqual(0, game.Score);
+        }
+
+        [Test]
+        public void OnePinEveryRoll()
+        {
+            rollMany(20, 1);
+            Assert.AreEqual(20, game.Score);
+        }
+
+        [Test]
+        public void Roll_PerfectGame()
+        {
+            rollMany(12, 10);
+            Assert.AreEqual(300, game.Score);
+        }
+
+        private void rollMany(int numRolls, int numPins)
+        {
+            for (int rollIteration = 1; rollIteration <= numRolls; rollIteration++)
+            {
+                game.Roll(numPins);
+            }
+        }
+    }
+}
